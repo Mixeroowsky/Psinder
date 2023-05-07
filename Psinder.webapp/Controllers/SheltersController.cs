b4c78a9f8d2eee274eec5567b0fdc218876ced62
Mixeroowsky <blackred519@gmail.com>
2023-05-07 17:12:59 +0200

    Controllers

---
+++ Psinder.webapp/Controllers/SheltersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Psinder.webapp.Controllers
+{
+    public class SheltersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
