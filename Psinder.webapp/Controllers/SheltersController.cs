daf57071b448f5b7aeba197d0a8209a99ef3b602
Mixeroowsky <blackred519@gmail.com>
2023-05-09 18:28:36 +0200

    Podstawowy widok stron

--- Psinder.webapp/Controllers/SheltersController.cs
+++ Psinder.webapp/Controllers/SheltersController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Psinder.Api.Models;
 
 namespace Psinder.webapp.Controllers
 {
     public class SheltersController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public SheltersController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _clientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index(string? name)
+        {
+            string uri;
+            if (string.IsNullOrEmpty(name))
+            {
+                ViewData["Title"] = "Wszystkie zwierzęta";
+                uri = "/Shelters/GetShelters/";
+            }
+            else
+            {
+                ViewData["Title"] = $"Zwierzęta o imieniu {name}";
+                uri = $"/Shelters/GetShelters/?name={name}";
+            }
+            HttpClient client = _clientFactory.CreateClient(name: "Psinder.Api");
+            HttpRequestMessage task = new(
+                method: HttpMethod.Get,
+                requestUri: uri
+            );
+            HttpResponseMessage response = await client.SendAsync(task);
+            IEnumerable<Shelter>? model = await response.Content.ReadFromJsonAsync<IEnumerable<Shelter>>();
+            return View(model);
         }
     }
 }
