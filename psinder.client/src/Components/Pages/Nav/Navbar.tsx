af6622a5876c3385c3e955b37441458a1389f512
Mixeroowsky <blackred519@gmail.com>
2024-11-27 20:16:55 +0100

    Plus sign on add button

--- psinder.client/src/Components/Pages/Nav/Navbar.tsx
+++ psinder.client/src/Components/Pages/Nav/Navbar.tsx
@@ -73,15 +73,40 @@ const Navbar = () => {
                 {isAuthenticated ? (
                   <>
                     {shelterRegistered ? (
-                      <Link to="/editShelter">
-                        <Button
-                          className={`${
-                            darkMode ? "bg-green-600" : "bg-green-400"
-                          } text-lg w-36 h-10 `}
-                        >
-                          View your shelter
-                        </Button>
-                      </Link>
+                      <>
+                        <Link to="/addPet">
+                          <Button
+                            className={`${
+                              darkMode ? "bg-green-600" : "bg-green-400"
+                            } text-lg w-36 h-10 gap-2`}
+                          >
+                            Add new pet
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-6 w-6"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M12 4v16m8-8H4"
+                              />
+                            </svg>
+                          </Button>
+                        </Link>
+                        <Link to="/editShelter">
+                          <Button
+                            className={`${
+                              darkMode ? "bg-green-600" : "bg-green-400"
+                            } text-lg w-36 h-10 `}
+                          >
+                            View your shelter
+                          </Button>
+                        </Link>
+                      </>
                     ) : (
                       <Link to="/addShelter">
                         <Button
