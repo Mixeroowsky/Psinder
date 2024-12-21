f79363407918e5b33f37df972a8e2b26882e3978
Mixeroowsky <blackred519@gmail.com>
2024-12-21 16:56:12 +0100

    Adjust condition for production

--- psinder.client/vite.config.ts
+++ psinder.client/vite.config.ts
@@ -6,40 +6,6 @@ import child_process from "child_process";
 import { env } from "process";
 import cookie from "cookie";
 
-const baseFolder =
-  env.APPDATA !== undefined && env.APPDATA !== ""
-    ? `${env.APPDATA}/ASP.NET/https`
-    : `${env.HOME}/.aspnet/https`;
-
-const certificateName = "psinder.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(baseFolder)) {
-  fs.mkdirSync(baseFolder, { recursive: true });
-}
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-  if (
-    0 !==
-    child_process.spawnSync(
-      "dotnet",
-      [
-        "dev-certs",
-        "https",
-        "--export-path",
-        certFilePath,
-        "--format",
-        "Pem",
-        "--no-password",
-      ],
-      { stdio: "inherit" }
-    ).status
-  ) {
-    throw new Error("Could not create certificate.");
-  }
-}
-
 const getHttpsConfig = () => {
   if (process.env.NODE_ENV === "production") {
     return {
@@ -49,6 +15,39 @@ const getHttpsConfig = () => {
       ),
     };
   } else {
+    const baseFolder =
+      env.APPDATA !== undefined && env.APPDATA !== ""
+        ? `${env.APPDATA}/ASP.NET/https`
+        : `${env.HOME}/.aspnet/https`;
+
+    const certificateName = "psinder.client";
+    const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+    const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+    if (!fs.existsSync(baseFolder)) {
+      fs.mkdirSync(baseFolder, { recursive: true });
+    }
+
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+      if (
+        0 !==
+        child_process.spawnSync(
+          "dotnet",
+          [
+            "dev-certs",
+            "https",
+            "--export-path",
+            certFilePath,
+            "--format",
+            "Pem",
+            "--no-password",
+          ],
+          { stdio: "inherit" }
+        ).status
+      ) {
+        throw new Error("Could not create certificate.");
+      }
+    }
     return {
       key: fs.readFileSync(keyFilePath),
       cert: fs.readFileSync(certFilePath),
